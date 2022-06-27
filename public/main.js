a5a14e276615d7fbc2a65090eff0095cd60ff943
rcurry7 <ryan.curry@outlook.com>
2022-06-27 15:07:07 +0100

    updated

--- public/main.js
+++ public/main.js
@@ -1,5 +1,59 @@
-const btn = document.getElementById('hello-btn')
+//const btn = document.getElementById('myfactbtn')
 
-btn.addEventListener('click', () => {
-  window.alert('Hello world!')
-})
+//btn.addEventListener('click', () => {
+
+  //document.getElementById("myart").style.display = "none";
+ 
+//})
+
+
+function showFact()
+{
+  document.getElementById("myfact").style.display = "block";
+  document.getElementById("mysoft").style.display = "none";
+  document.getElementById("myex").style.display = "none";
+  document.getElementById("myam").style.display = "none";
+  document.getElementById("myhob").style.display = "none";
+
+
+}
+
+function showSoft()
+{
+  document.getElementById("myfact").style.display = "none";
+  document.getElementById("mysoft").style.display = "block";
+  document.getElementById("myex").style.display = "none";
+  document.getElementById("myam").style.display = "none";
+  document.getElementById("myhob").style.display = "none";
+
+}
+
+function showEx()
+{
+  document.getElementById("myfact").style.display = "none";
+  document.getElementById("mysoft").style.display = "none";
+  document.getElementById("myex").style.display = "block";
+  document.getElementById("myam").style.display = "none";
+  document.getElementById("myhob").style.display = "none";
+
+}
+
+function showAmb()
+{
+  document.getElementById("myfact").style.display = "none";
+  document.getElementById("mysoft").style.display = "none";
+  document.getElementById("myex").style.display = "none";
+  document.getElementById("myam").style.display = "block";
+  document.getElementById("myhob").style.display = "none";
+
+}
+
+function showHob()
+{
+  document.getElementById("myfact").style.display = "none";
+  document.getElementById("mysoft").style.display = "none";
+  document.getElementById("myex").style.display = "none";
+  document.getElementById("myam").style.display = "none";
+  document.getElementById("myhob").style.display = "block";
+
+}
