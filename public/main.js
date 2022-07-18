e31c538f83a32740c8af5e3808a1fa4c98443227
rcurry7 <rcurry87@gmail.com>
2022-07-18 09:22:41 +0100

    update

--- public/main.js
+++ public/main.js
@@ -14,5 +14,7 @@ let getClasses = document.getElementsByClassName("animate-opacity");   //get all
   for(let i = 0; i < getClasses.length; i++){  //loop through list
     getClasses[i].style.display = "none";   //for each of the items (i) in list - display none
   }
-document.getElementById(idName).style.display = "block"; //display the one you want
+document.getElementById(idName).style.display = "block"; //display the one you want /
+
 }
+
