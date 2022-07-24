b9124c08061d4ecf7970e35933ce9c8bfef6c823
rcurry7 <rcurry87@gmail.com>
2022-07-24 07:20:21 +0100

    new updates

--- public/main.js
+++ public/main.js
@@ -1,10 +1,54 @@
-//const btn = document.getElementById("fact")
 
-//btn.addEventListener('click', () => {
 
- // document.getElementById("myfact").style.display = "block";
- //
-//})
+const hamburger = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".navmenu");
+
+//scroll hide navbar
+const body = document.body;
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+
+
+  let currentScroll = window.pageYOffset;
+
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up")
+   
+  };
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up")
+    body.classList.add("scroll-down")
+    
+  }
+
+  if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+    body.classList.add("scroll-up")
+    body.classList.remove("scroll-down")
+    
+  }
+  
+  lastScroll = currentScroll;
+
+}
+)
+
+
+hamburger.addEventListener("click", ()=> {
+  hamburger.classList.toggle("active");
+  navmenu.classList.toggle("active");
+ })
+
+ document.querySelectorAll(".navlink").forEach(nl => nl.  //for each click on each nav link, remove active from class list.
+  addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navmenu.classList.remove("active");
+  })
+  )
+
+
 
 
 function showHide(idName){
